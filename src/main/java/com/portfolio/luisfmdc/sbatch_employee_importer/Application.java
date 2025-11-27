e9751812dc2ab35e5555bf05469a922b5a600d4d
Luis Felipe Maia <lf.mcosta23@gmail.com>
2025-11-26 23:09:41 -0300

    Estrutura inicial do projeto sbatch-employee-importer.

---
+++ src/main/java/com/portfolio/luisfmdc/sbatch_employee_importer/Application.java
@@ -0,0 +1,13 @@
+package com.portfolio.luisfmdc.sbatch_employee_importer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+    static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/portfolio/luisfmdc/sbatch_employee_importer/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.portfolio.luisfmdc.sbatch_employee_importer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
